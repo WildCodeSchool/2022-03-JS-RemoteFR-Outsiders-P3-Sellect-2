13a8fa06a924ec47497952f07a1c24a3bbb9aede
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-06-14 15:26:07 +0200

    bloc 1 and css ok

---
+++ frontend/src/components/Bloc1.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "@assets/common.css";
+import "@assets/Bloc1.css";
+import bloc1 from "@assets/img/bloc1.jpg";
+
+export default function Bloc1() {
+  return (
+    <section className="bloc1_container">
+      <div className="bloc1_left">
+        <h1>GAGNEZ EN POUVOIR D’ACHAT !</h1>
+        <h2>
+          Identifiez et revalorisez vos dépenses à travers un Audit et un
+          accompagnement humain.
+        </h2>
+        <p>
+          <span>La seule dépense</span> qui fait baisser toutes les autres
+        </p>
+        <div className="btn">
+          <button type="button">Réserver mon audit</button>
+          <button type="button">Je deviens apporteur d'affaires</button>
+        </div>
+      </div>
+      <div className="bloc1_right">
+        <img src={bloc1} alt="photo_contrat" />
+      </div>
+    </section>
+  );
+}
