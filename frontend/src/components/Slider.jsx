140d2bd8592189bf18136416db77b847ebfcd468
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-06-15 14:23:21 +0200

    slider ok

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import logosellect from "../assets/img/logosellect.svg";
+import slidepartner from "../assets/img/slidepartner.svg";
+import tips from "../assets/img/tips.svg";
 import "../assets/Slider.css";
 
 export default function Slider() {
   return (
-    <div className="slider-div">
+    <div className="carousel-div">
+      <img className="img1" src={tips} alt="tips" />
       <Carousel>
-        <div className="carousel-div">
-          <img src={logosellect} alt="sellectlogo" />
+        <div className="img-carousel-div">
+          <img src={slidepartner} alt="sellectlogo" />
           <div className="overlay">
-            <h2 className="overlay_title"> Immobilier</h2>
-            <p className="overlay_text"> Bien immobiler </p>
+            <h2 className="overlay_title"> Partenaire N°1</h2>
+            <p className="overlay_text"> Contrôle technique </p>
           </div>
         </div>
-        <div className="carousel-div">
-          <img src={logosellect} alt="sellectlogo" />
+        <div className="img-carousel-div">
+          <img src={slidepartner} alt="sellectlogo" />
           <div className="overlay">
-            <h2 className="overlay_title"> Immobilier</h2>
+            <h2 className="overlay_title"> Partenaire N°2</h2>
             <p className="overlay_text"> Immobilier moins cher</p>
           </div>
         </div>
-        <div className="carousel-div">
-          <img src={logosellect} alt="sellectlogo" />
+        <div className="img-carousel-div">
+          <img src={slidepartner} alt="sellectlogo" />
           <div className="overlay">
             <h2 className="overlay_title"> Immobilier</h2>
             <p className="overlay_text"> Bien immobiler </p>
