476e00896b740151f6259546615cd48ea2c6090a
Abdallah_Mesbahi <a.mesbahi92@gmail.com>
2022-06-15 17:40:27 +0200

    replace bloc2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,14 +6,13 @@ import Bloc2 from "@components/Bloc2";
 import HomeBlock3 from "@components/HomeBlock3";
 import Bloc1 from "@components/Bloc1";
 
-
 function Home() {
   return (
     <div className="content_container">
       <Navbar />
-      <Bloc2 />
       <div className="content">
         <Bloc1 />
+        <Bloc2 />
         <HomeBlock3 />
       </div>
       <Footer />
