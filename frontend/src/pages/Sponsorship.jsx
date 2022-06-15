c6b8c98f135b967529c7c735aadb9e4fcf98db78
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-06-15 17:17:46 +0200

    ajout puces sur parrainage et calendrier

--- frontend/src/pages/Sponsorship.jsx
+++ frontend/src/pages/Sponsorship.jsx
@@ -17,29 +17,29 @@ export default function Sponsorship() {
           <div className="instruction1">
             <h1>1 - Obtenez votre code</h1>
             <p>
-              Réalisez votre <span>audit personnalisé</span>
+              ► Réalisez votre <span>audit personnalisé</span>
             </p>
             <p>
-              Si vous souhaitez{" "}
+              ► Si vous souhaitez{" "}
               <span> partager la philosophie de l’association</span> et
               améliorer le pouvoir d’achat d'autrui
             </p>
             <p>
-              <span>Un code</span> vous sera remis à la remise de votre Audit.
+              <span>► Un code</span> vous sera remis à la remise de votre Audit.
             </p>
           </div>
 
           <div className="instruction2">
             <h1>2 - Transmettez votre code</h1>
             <p>
-              Nous offrons <span>10 euros</span> par filleul
+              ► Nous offrons <span>10 euros</span> par filleul
             </p>
           </div>
 
           <div className="instruction3">
             <h1>3 - Versement</h1>
             <p>
-              Ce montant sera <span>directement versé sur votre compte</span>{" "}
+              ► Ce montant sera <span>directement versé sur votre compte</span>{" "}
               dès la fin de réalisation de l'audit de votre filleul.
             </p>
           </div>
