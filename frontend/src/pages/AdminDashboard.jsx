0dfe51cb27065e5a49f1f9dab2afb4c6db487ab4
AUDREN FLOCH <a.floch@outlook.fr>
2022-06-21 10:53:37 +0200

    searchbar for admin dashboard + logout button

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -6,6 +6,7 @@ import Footer from "@components/Footer";
 
 function AdminDashboard() {
   const [users, setUsers] = useState([]);
+  const [searchUser, setSearchUser] = useState("");
 
   const getUsers = () => {
     axios
@@ -22,15 +23,29 @@ function AdminDashboard() {
     <div>
       <Navbar />
       <h2>Liste Utilisateurs</h2>
+      <input
+        type="text"
+        placeholder="Rechercher un utilisateur"
+        value={searchUser}
+        onChange={(e) => setSearchUser(e.target.value)}
+      />
       <ul>
         {users &&
-          users.map((user) => {
-            return (
-              <li key={user.id}>
-                <UserCard user={user} />
-              </li>
-            );
-          })}
+          users
+            .filter(
+              (user) =>
+                user.firstname.includes(searchUser) ||
+                user.lastname.includes(searchUser) ||
+                user.email.includes(searchUser) ||
+                user.phoneNumber.includes(searchUser)
+            )
+            .map((user) => {
+              return (
+                <li key={user.id}>
+                  <UserCard user={user} />
+                </li>
+              );
+            })}
       </ul>
       <Footer />
     </div>
