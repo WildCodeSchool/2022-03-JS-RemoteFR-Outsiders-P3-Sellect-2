8c74b563174ad1887d9fdfdd688b9e6c246564f4
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-06-22 17:13:49 +0200

    create wilders page

--- frontend/src/pages/Wilders.jsx
+++ frontend/src/pages/Wilders.jsx
@@ -5,6 +5,7 @@ import audrensellect from "../assets/img/audrensellect.jpg";
 import logosellect from "../assets/img/logosellect.svg";
 import gloriasellect from "../assets/img/gloriasellect.jpg";
 import sellect2 from "../assets/img/sellect2.svg";
+import logowild from "../assets/img/logowild.png";
 import "../assets/Footer.css";
 
 export default function Wilders() {
@@ -14,11 +15,20 @@ export default function Wilders() {
         <Link to="/">
           <img src={sellect2} className="login-logo" alt="sellect-logo" />
         </Link>
-        <h2> Les développeurs de SELLECT </h2>
+        <h2>
+          {" "}
+          <span>
+            {" "}
+            <img src={logowild} alt="wild logo" className="img-dev" />{" "}
+          </span>{" "}
+          <br />
+          Les développeurs de SELLECT{" "}
+        </h2>
         <p>
           {" "}
           Nous avons voulu un site web performant et pratique pour Sellect.
         </p>
+        <p> Réalisée par une équipe motivée </p>
       </div>
 
       <div className="div-img-wilders">
