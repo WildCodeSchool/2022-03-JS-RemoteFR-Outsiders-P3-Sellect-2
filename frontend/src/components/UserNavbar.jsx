4b06e0f17277365b7ea668658ef060c70e99f5dc
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-07-07 21:15:06 +0200

    checkout branch

--- frontend/src/components/UserNavbar.jsx
+++ frontend/src/components/UserNavbar.jsx
@@ -1,108 +1,41 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-// import sellect2 from "../assets/img/sellect2.svg";
-import logosellect from "../assets/img/logosellect.svg";
-import toggle from "../assets/img/toggle.svg";
-import "../assets/Navbar.css";
-import "../assets/common.css";
 import Logout from "./Logout";
+import logosellect from "../assets/img/logosellect.svg";
+import "../assets/UserNavbar.css";
 
 export default function UserNavbar() {
-  const [toggleMenu, setToggleMenu] = useState(false);
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  const toggleNav = () => {
-    setToggleMenu(!toggleMenu);
-  };
-
-  useEffect(() => {
-    const changeWidth = () => {
-      setScreenWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", changeWidth);
-
-    return () => {
-      window.removeEventListener("resize", changeWidth);
-    };
-  }, []);
-
-  /* function handleClick(e) {
-    e.preventDefault();
-    console.warn("Le lien a été cliqué.");
-  } */
-
   return (
-    <>
-      <div className="div-nav">
-        <div className="div-sellectlogo">
-          <img
-            src={logosellect}
-            alt="sellectlogo"
-            className="img-sellectlogo"
-          />
-        </div>
-        <div className="div-button">
-          <button type="button" onClick={toggleNav} className="btn-toggle">
-            {" "}
-            <img src={toggle} alt="menu hamburger" className="img-hamburger" />
-          </button>
-        </div>
+    <div className="container-usernavbar">
+      <div className="div-sellectlogo">
+        <img src={logosellect} alt="sellectlogo" className="img-sellectlogo" />
       </div>
-
-      <div className="div-ul">
-        {(toggleMenu || screenWidth > 750) && (
-          <>
-            <div>
-              <nav className="liste-nav">
-                <NavLink
-                  to="/mon-compte"
-                  className={(items) =>
-                    items.isActive ? "items-activated" : "items"
-                  }
-                >
-                  Accueil
-                </NavLink>
-
-                <NavLink
-                  to="/mon-compte/mettre-a-jour"
-                  className={(items) =>
-                    items.isActive ? "items-activated" : "items"
-                  }
-                >
-                  Profil
-                </NavLink>
-              </nav>
-            </div>
-
-            {localStorage.getItem("loggedIn") ? (
-              <div className="btn-div">
-                <Logout />
-              </div>
-            ) : (
-              <div className="btn-div">
-                <NavLink to="/inscription" className="items">
-                  <button
-                    className="button-member"
-                    type="button"
-                    /* onClick={handleClick} */
-                  >
-                    Devenir membre
-                  </button>
-                </NavLink>
-                <NavLink to="/connexion" className="items">
-                  <button
-                    className="button-connect"
-                    type="button"
-                    /* onClick={handleClick} */
-                  >
-                    Se connecter
-                  </button>
-                </NavLink>
-              </div>
-            )}
-          </>
-        )}
-      </div>
-    </>
+      {localStorage.getItem("loggedIn") ? (
+        <div className="btn-div">
+          <Logout />
+        </div>
+      ) : (
+        <div className="btn-div">
+          <NavLink to="/inscription" className="items">
+            <button
+              className="button-member"
+              type="button"
+              /* onClick={handleClick} */
+            >
+              Devenir membre
+            </button>
+          </NavLink>
+          <NavLink to="/connexion" className="items">
+            <button
+              className="button-connect"
+              type="button"
+              /* onClick={handleClick} */
+            >
+              Se connecter
+            </button>
+          </NavLink>
+        </div>
+      )}
+    </div>
   );
 }
