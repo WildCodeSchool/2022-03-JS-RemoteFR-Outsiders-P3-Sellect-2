17b6e8a78a86133d63125bedc145ebcff2d1c1bb
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-07-12 12:32:51 +0200

    update css fileCard

--- frontend/src/components/FileCard.jsx
+++ frontend/src/components/FileCard.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import "../assets/common.css";
+import "../assets/Filecard.css";
 
 function FileCard({ file }) {
   return (
-    <div>
-      <p>Nom:&nbsp;{file.name}</p>
-      {file.category !== "Compte-rendu d'audit" && (
-        <p>Catégorie:&nbsp;{file.category}</p>
-      )}
-      <p>Date d'envoi:&nbsp;{file.sendDate}</p>
-      <a href={`../../../backend/uploads/${file.content}`} download={file.name}>
-        Télécharger
-      </a>
+    <div className="filecard">
+      <details className="filecard_details">
+        <summary className="filecard_summary">
+          <p>&nbsp;{file.name}</p>
+        </summary>
+        {file.category !== "Compte-rendu d'audit" && (
+          <p>Catégorie:&nbsp;{file.category}</p>
+        )}
+        <p>Date d'envoi:&nbsp;{file.sendDate}</p>
+        <div className="filecard_link">
+          <a href={`../../../backend/uploads/${file.content}`} target="__blank">
+            Visualiser
+          </a>
+          <a
+            href={`../../../backend/uploads/${file.content}`}
+            download={file.name}
+          >
+            Télécharger
+          </a>
+        </div>
+      </details>
     </div>
   );
 }
